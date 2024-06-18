90b81c6f5bbf7e00516ca5fb41f76e09c1776a60
Paul-Daniel Pretorius <165285046+PaulDPretorius@users.noreply.github.com>
2024-06-18 04:16:41 +0200

    Final Commit

--- get-swole/src/App.jsx
+++ get-swole/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoriteRecipePage from "./pages/FavoriteRecipePage";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="flex">
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/favorites' element={<FavoriteRecipePage />} /> 
